c3c03ad533b1bcb00ac30e3b828671ad03671bbf
Taiwan Easterling <t_east8@Taiwans-MacBook-Pro.local>
2016-08-01 16:39:15 -0700

    made changes to my img names and got images to show

--- js/app.js
+++ js/app.js
@@ -20,21 +20,20 @@ function catalogItem (imageName, filePath) {
 }
 
 new catalogItem('Luggage Bag', 'img/bag.jpg');
-new catalogItem('Luggage Bag', 'img/bag.jpg');
-new catalogItem('Luggage Bag', 'img/bag.jpg');
 new catalogItem('Banana Cuts', 'img/banana.jpg');
 new catalogItem('Bathroom Utility', 'img/bathroom.jpg');
 new catalogItem('Open Boots', 'img/boots.jpg');
-new catalogItem('Breakfast Maker', 'img/breakfast img/breakfast.jpg');
-new catalogItem('Banana Cuts', 'img/bubblegum.jpg');
+new catalogItem('Breakfast Maker', 'img/breakfast.jpg');
+new catalogItem('Meatball Gum', 'img/bubblegum.jpg');
 new catalogItem('Red Chair', 'img/chair.jpg');
-new catalogItem('Monster', 'img/cthulu.jpg');
+new catalogItem('Monster', 'img/cthulhu.jpg');
 new catalogItem('Duckdog', 'img/dog-duck.jpg');
 new catalogItem('Utensil Pens', 'img/dragon.jpg');
 new catalogItem('Pet Sweeper', 'img/pen.jpg');
-new catalogItem('Pizza Scissors', 'img/breakfast img/pet-sweep.jpg');
-new catalogItem('Shark Tank', 'img/scissors.jpg');
-new catalogItem('Baby Sweeper', 'img/shark.jpg');
+new catalogItem('Pet Removal', 'img/pet-sweep.jpg');
+new catalogItem('Pizza Scissors', 'img/scissors.jpg');
+new catalogItem('Shark Tank', 'img/shark.jpg');
+new catalogItem('Baby Sweeper', 'img/sweep.jpg');
 new catalogItem('Taun Bag', 'img/tauntaun.jpg');
 new catalogItem('Unicorn Meat', 'img/unicorn.jpg');
 new catalogItem('Usb Tenicle', 'img/usb.jpg');
@@ -42,15 +41,14 @@ new catalogItem('Water Can', 'img/water-can.jpg');
 new catalogItem('Wine-Glass', 'img/wine-glass.jpg');
 
 
-var randomindex1 = [Math.floor(Math.random() * catalogArray.length);
-var randomindex2 = [Math.floor(Math.random() * catalogArray.length);
-var randomindex3 = [Math.floor(Math.random() * catalogArray.length);
+// while (randomindex2 === randomindex1) {
+//   randomindex2 = [Math.floor(Math.random() * imageArray.length)
+//   }
 
-while randomindex2 === randomindex1) {
-  randomindex2 = [Math.floor(Math.random() * catalogArray.length);
-}
+  var randomindex1 = Math.floor(Math.random() * imageArray.length);
+  var randomindex2 = Math.floor(Math.random() * imageArray.length);
+  var randomindex3 = Math.floor(Math.random() * imageArray.length);
 
-
-leftImg.src = catalogArray[randomindex1].filePath;
-centerImg.src = catalogArray[randomindex3].filePath;
-rightImg.src = catalogArray[randomindex2].filePath;
+leftImg.src = imageArray[randomindex1].filePath;
+centerImg.src = imageArray[randomindex3].filePath;
+rightImg.src = imageArray[randomindex2].filePath;
